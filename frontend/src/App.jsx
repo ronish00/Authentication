6d6cd35633cb2fadc4c11f367ec5f8ccecb5c5fd
Ronish Katuwal <ronishkatuwal00@gmail.com>
2024-10-07 21:20:16 +0545

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from './pages/register'
+import Login from './pages/Login';
 
 function App() {
 
   return (
-    <>
-      <Register />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element="" >
+          <Route index path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,35 +1,23 @@
-import React from "react";
+import React from 'react'
 import Input from "../components/Input";
 import bg from "../images/bg.jpg";
 import { Link } from "react-router-dom";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="relative w-full h-screen grid grid-cols-2">
       <div className="p-24 bg-black bg-opacity-70">
-        <p className="font-medium text-sm text-gray-200">START FOR FREE</p>
+        <p className="font-medium text-sm text-gray-200">WELCOME BACK</p>
         <h1 className="text-5xl text-white font-bold my-5">
-          Create new account<span className="text-blue-400">.</span>
+          Login To Your Account<span className="text-blue-400">.</span>
         </h1>
         <p className="text-gray-300">
-          Already a member?{" "}
-          <Link to="/login" className="text-blue-400">
-            Log In
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-400">
+            Sign up
           </Link>
         </p>
-        <form className="mt-8 grid grid-cols-2 gap-8 w-full">
-          <Input
-            placeholder="Enter a first name"
-            type="text"
-            name="firstName"
-            label="First Name"
-          />
-          <Input
-            placeholder="Enter a last name"
-            type="text"
-            name="lastName"
-            label="Last Name"
-          />
+        <form className="mt-8 flex flex-col gap-8 w-full">
           <Input
             placeholder="Enter a email"
             type="email"
@@ -43,13 +31,13 @@ const Register = () => {
             label="Password"
           />
           <button className="bg-blue-500 py-3 rounded col-span-2 text-white">
-            Create Account
+            Login
           </button>
         </form>
       </div>
       <img src={bg} className="absolute w-full h-full top-0 left-0 -z-10 -scale-x-100 " />
     </div>
-  );
-};
+  )
+}
 
-export default Register;
+export default Login
